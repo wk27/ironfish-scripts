9e96382679753e7dab8f219d17ca2b1f39e3efd3
Vladimir <wk@aletezo.ru>
2022-05-15 21:50:17 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -11,7 +11,7 @@ while true; do
 BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }')
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
-	echo -e $(date): '\033[1;32m'"Transaction:"'\033[0m'
+	echo -e '\033[1;32m'"Transaction:"'\033[0m'
 	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
 	/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm
 	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
