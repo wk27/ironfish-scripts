82923f44ab66a8a472967965ec261168ec76bb58
Vladimir <wk@aletezo.ru>
2022-05-15 22:36:00 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -12,7 +12,6 @@ BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:b
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	echo -e '\033[1;32m'"Transaction:"'\033[0m'
-	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
 	/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm
 	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
 else
