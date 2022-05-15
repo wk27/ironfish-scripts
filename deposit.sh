2e3f4bbd863825a0d93a11452923fefb5c7528e8
Vladimir <wk@aletezo.ru>
2022-05-15 21:00:10 +0300

    small fix

--- deposit.sh
+++ deposit.sh
@@ -6,15 +6,16 @@
 # background start:
 # nohup bash /root/deposit.sh >> /var/log/ironfish_deposit.log &
 #
-dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi;
+dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
 while true; do
-BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }');
+BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }')
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	echo -e $(date): '\033[1;32m'"Transaction:"'\033[0m'
 	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
-	/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm;
+	/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm
 	echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
-fi;
-sleep 61;
+else
+	sleep 60
+fi
 done
