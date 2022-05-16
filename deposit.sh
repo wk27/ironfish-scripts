3c83909413cac1d016beaaa7d0f68e37a9b3d1d9
Vladimir <wk@aletezo.ru>
2022-05-17 00:19:41 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -8,7 +8,7 @@
 #
 dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
 while true; do
-BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }')
+BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	echo -e '\033[1;32m'"Transaction:"'\033[0m'
