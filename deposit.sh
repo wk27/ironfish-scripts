91c8c421d836b4066498f771e44081747be8a075
VV <wk@aletezo.ru>
2022-05-17 13:36:21 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -23,7 +23,8 @@ echo $$ > /var/run/${filename}.pid
 dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
 
 while true; do
-BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')
+# BALANCE=$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')
+BALANCE=`/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//'`
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	REPEAT=$(echo ${BALANCE}/0.10000001 | bc -l | cut -d '.' -f1)
