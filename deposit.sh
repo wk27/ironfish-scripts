87023c1566ff3f129897e2a2225ee2066dfe2cb3
VV <wk@aletezo.ru>
2022-05-18 11:57:54 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -13,14 +13,14 @@ if [ ${filename} != "deposit.sh" ]; then
 fi
 
 if [[ `pgrep -f ${filename}` != "$$" ]]; then
-        echo "Another instance of the script already exist! Exiting"
+	echo "Another instance of the script already exist! Exiting"
 	if [ -s "/var/run/${filename}.pid" ]; then echo -e "You can try to \033[0;31m kill -9 $(cat /var/run/${filename}.pid) \033[0m for killing the current process and then run a new one"; fi
-        exit 1
+	exit 1
 fi
 
 echo $$ > /var/run/${filename}.pid
 
-dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc; fi
+dpkg -s bc > /dev/null 2>&1; if [ "$(echo $?)" != "0" ]; then apt-get -y install bc > /dev/null 2>&1; echo "Note: bc package has been installed"; fi
 
 while true; do
 BALANCE="$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance $IRONFISH_WALLET | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')"
@@ -30,15 +30,20 @@ if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	if [ ! -z "${REPEAT}" ]; then
 		for i in `seq ${REPEAT}`; do
 			echo -e '\033[1;32m'"Transaction:"'\033[0m'
-			/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm | tee /tmp/deposit-last.log
+			/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ start deposit --confirm 2>&1 | tee /tmp/deposit-last.log
 			echo -e '\033[0;31m'"-------------------------------------------------------------"'\033[0m'
 			if [ ! -z "$(egrep -i "Insufficient" /tmp/deposit-last.log)" ]; then
 				sleep 360
 				break
 			fi
+			if [ ! -z "$(egrep -i "An error occurred while sending the transaction" /tmp/deposit-last.log)" ]; then
+				# It means that network is down, script will sleep for 30 minutes until next try
+				sleep 1800
+				break
+			fi
 		done
 	fi
 else
-	sleep 10
+	sleep 15
 fi
 done
