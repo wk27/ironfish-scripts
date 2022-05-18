90bbf2247e2d3f69372c51f7da96776be3fd45cf
VV <wk@aletezo.ru>
2022-05-18 20:51:04 +0300

    Update deposit.sh

--- deposit.sh
+++ deposit.sh
@@ -35,7 +35,7 @@ if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
 	REPEAT=$(echo ${BALANCE}/0.10000001 | bc -l | cut -d '.' -f1)
 	if [ ! -z "${REPEAT}" ]; then
 		for i in `seq ${REPEAT}`; do
-			if [ "$(($i % 10))" == 0 ] || [ "$i" != "${REPEAT}" ]; then
+			if [ "$(($i % 10))" == 0 ] || [ "$i" != "1" ]; then
 				echo $(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance ${IRONFISH_WALLET} | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//') > /tmp/.shadow_balance 2>&1 &
 				echo -e $(date): '\033[1;32m'Possible balance amount is about $(cat /tmp/.shadow_balance)'\033[0m'
 			fi
