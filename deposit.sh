fbd4b144180ae3c0984d4b4a52fa12950d338722
VV <wk@aletezo.ru>
2022-06-03 14:56:17 +0300

    LC_MONETARY for ru_RU locale fixed

--- deposit.sh
+++ deposit.sh
@@ -30,7 +30,11 @@ echo -e "Your wallet name is $IRONFISH_WALLET \nYour node name is ${IRONFISH_NOD
 
 while true; do
 INSUFFICIENT_COUNT=0
+if [[ "$(locale | grep LC_MONETARY)" =~ "ru_RU" ]]; then
+BALANCE="$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance ${IRONFISH_WALLET} | egrep "Amount available to spend" | awk '{ print $6 }' | sed -E 's/(.*),/\1./' | sed 's/\,//')"
+	else
 BALANCE="$(/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish accounts:balance ${IRONFISH_WALLET} | egrep "Amount available to spend" | awk '{ print $6 }' | sed 's/\,//')"
+fi
 echo ${BALANCE} > /tmp/.shadow_balance
 echo -e $(date): '\033[1;32m'"Available balance is ${BALANCE}"'\033[0m'
 if (( $(echo "${BALANCE} >= 0.10000001" | bc -l) )); then
