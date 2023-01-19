4d21a8dfbbafdf2f9d43c3bccfe1bdd1fa5d7716
VV <wk@aletezo.ru>
2023-01-19 23:42:36 +0300

    Update deposit_testnet3.sh

--- deposit_testnet3.sh
+++ deposit_testnet3.sh
@@ -10,7 +10,7 @@ id="$(${ironfish_cmd} config:get blockGraffiti | sed -n '3p' | sed -e "s/^.//;s/
 
 ${ironfish_cmd} wallet:mint -f=${wallet} -a=100 -n=${id} -m=${id} -o=${fee} -v --confirm
 
-echo -e $(date): '\033[1;32m'Need to wait the network... Do not close the script.'\033[0m'
+echo -e $(date): '\033[1;32m' > Need to wait the network... Do not close the script.'\033[0m'
 echo
 
 sleep 1337
@@ -19,9 +19,12 @@ asset_id="$(${ironfish_cmd} wallet:balances | grep ${id} | awk '{ print $2 }')"
 
 ${ironfish_cmd} wallet:burn -a=90 -f=${wallet} -o=${fee} -i=${asset_id} --confirm
 
-echo -e $(date): '\033[1;32m'Need to wait the network... Do not close the script.'\033[0m'
+echo -e $(date): '\033[1;32m' > Need to wait the network... Do not close the script.'\033[0m'
 echo
 
 sleep 666
 
 ${ironfish_cmd} wallet:send -i=${asset_id} -a=1 -t=dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca -f=${wallet} -o=${fee} --memo=${id} --confirm
+
+echo -e $(date): '\033[1;32m'_________________________________________'\033[0m'
+echo
