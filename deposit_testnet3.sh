d63c9fb72e42454994d2870c4ae51d506bb415bb
VV <wk@aletezo.ru>
2023-01-20 16:12:38 +0300

    Update deposit_testnet3.sh

--- deposit_testnet3.sh
+++ deposit_testnet3.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# ironfish_cmd="ironfish"                                                  # use for NPM
-ironfish_cmd="/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish" # use for SRC
+# ironfish_cmd="ironfish"                                                      # use for NPM
+# id="$(${ironfish_cmd} config:get blockGraffiti | sed -e "s/^.//;s/.$//")"    # use for NPM
+
+ironfish_cmd="/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish"              # use for SRC
+id="$(${ironfish_cmd} config:get blockGraffiti | sed -n '3p' | sed -e "s/^.//;s/.$//")" # use for SRC
 
 fee="0.00001337"
 wallet="default"
@@ -10,7 +13,7 @@ id="$(${ironfish_cmd} config:get blockGraffiti | sed -n '3p' | sed -e "s/^.//;s/
 
 ${ironfish_cmd} wallet:mint -f=${wallet} -a=100 -n=${id} -m=${id} -o=${fee} -v --confirm
 
-echo -e $(date): '\033[1;32m' > Need to wait the network... Do not close the script.'\033[0m'
+echo -e $(date): '\033[1;32m' Need to wait the network... Do not close the script.'\033[0m'
 echo
 
 sleep 1337
@@ -19,7 +22,7 @@ asset_id="$(${ironfish_cmd} wallet:balances | grep ${id} | awk '{ print $2 }')"
 
 ${ironfish_cmd} wallet:burn -a=90 -f=${wallet} -o=${fee} -i=${asset_id} --confirm
 
-echo -e $(date): '\033[1;32m' > Need to wait the network... Do not close the script.'\033[0m'
+echo -e $(date): '\033[1;32m' Need to wait the network... Do not close the script.'\033[0m'
 echo
 
 sleep 666
