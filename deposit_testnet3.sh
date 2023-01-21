61cc35c355b8ded399437dc261320936c40ae511
VV <wk@aletezo.ru>
2023-01-21 15:47:25 +0300

    Update deposit_testnet3.sh

--- deposit_testnet3.sh
+++ deposit_testnet3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# ironfish_cmd="ironfish"                                                      # use for NPM
-# id="$(${ironfish_cmd} config:get blockGraffiti | sed -e "s/^.//;s/.$//")"    # use for NPM
+# ironfish_cmd="ironfish"                                                               # use for NPM
+# id="$(${ironfish_cmd} config:get blockGraffiti | sed -e "s/^.//;s/.$//")"             # use for NPM
 
 ironfish_cmd="/usr/bin/yarn --cwd ${HOME}/ironfish/ironfish-cli/ ironfish"              # use for SRC
 id="$(${ironfish_cmd} config:get blockGraffiti | sed -n '3p' | sed -e "s/^.//;s/.$//")" # use for SRC
@@ -20,14 +20,14 @@ sleep 1337
 
 asset_id="$(${ironfish_cmd} wallet:balances | grep ${id} | awk '{ print $2 }')"
 
-${ironfish_cmd} wallet:burn -a=90 -f=${wallet} -o=${fee} -i=${asset_id} --confirm
+${ironfish_cmd} wallet:burn -a=90 -f=${wallet} -o=${fee} --assetId=${asset_id} --confirm
 
 echo -e $(date): '\033[1;32m' Need to wait the network... Do not close the script.'\033[0m'
 echo
 
 sleep 666
 
-${ironfish_cmd} wallet:send -i=${asset_id} -a=1 -t=dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca -f=${wallet} -o=${fee} --memo=${id} --confirm
+${ironfish_cmd} wallet:send --assetId=${asset_id} -a=1 -t=dfc2679369551e64e3950e06a88e68466e813c63b100283520045925adbe59ca -f=${wallet} -o=${fee} --memo=${id} --confirm
 
 echo -e $(date): '\033[1;32m'_________________________________________'\033[0m'
 echo
